69947677b83107dba02b8282bcdea1b99e67f269
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2022-05-09 18:59:48 +0600

    google login complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+
+const app = initializeApp(firebaseConfig);
 
 function App() {
+
+  const auth = getAuth();
+  const googleProvider = new GoogleAuthProvider();
+
+  const [user, setUser] = useState({});
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        const user = result.user;
+        console.log(user);
+        setUser(user);
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
+        console.log(errorCode, errorMessage, email);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn}>Sign In Using Google</button>
+      <h3>Email: {user.email}</h3>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
